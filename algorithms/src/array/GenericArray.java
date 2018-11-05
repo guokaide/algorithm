703472d355dd4d0539c72c9d07b99da47fee40fa
guokaide <kaideguo@gmail.com>
2018-11-05 17:24:03 +0800

    add solultion 1

--- algorithms/src/array/GenericArray.java
+++ algorithms/src/array/GenericArray.java
@@ -4,16 +4,17 @@ package array;
  * 泛型动态数组
  *
  * @param <T>
+ *
  */
 
 public class GenericArray<T> {
     private T[] data;
-    private int size;
+    private int size; //数组当前元素个数,保证其可用范围为[0, size]
 
     // 根据传入容量，构造Array
     public GenericArray(int capacity) {
-        data = (T[]) new Object[capacity];
-        size = 0;
+        this.data = (T[]) new Object[capacity];
+        this.size = 0;
     }
 
     // 无参构造方法，默认数组容量为10
@@ -23,73 +24,110 @@ public class GenericArray<T> {
 
     // 获取数组容量
     public int getCapacity() {
-
+        return data.length;
     }
 
-    // 获取当前元素个数
-    public int count() {
-
+    // 获取当前数组元素个数
+    public int getSize() {
+        return size;
     }
 
     // 判断数组是否为空
     public boolean isEmpty() {
-
+        return size == 0;
     }
 
     // 修改 index 位置的元素
     public void set(int index, T e) {
-
+        checkIndex(index);
+        data[index] = e;
     }
 
     // 获取对应 index 位置的元素
     public T get(int index) {
-
+        checkIndex(index);
+        return data[index];
     }
 
     // 查看数组是否包含元素e
     public boolean contains(T e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {   // 注意这里
+                return true;
+            }
+        }
 
+        return false;
     }
+
 
     // 获取对应元素的下标, 未找到，返回 -1
     public int find(T e) {
-
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 
     // 在 index 位置，插入元素e, 时间复杂度 O(m+n)
     public void add(int index, T e) {
-
+        checkIndex(index);
+        if (size == data.length) {
+            resize(2 * size);
+        }
+        for (int i = index; i < size; i++) {
+            data[i+1] = data[i];
+        }
+        data[index] = e;
+        size++;
     }
 
     // 向数组头插入元素
     public void addFirst(T e) {
-
+        add(0, e);
     }
 
     // 向数组尾插入元素
     public void addLast(T e) {
-
+        add(size, e);
     }
 
     // 删除 index 位置的元素，并返回
     public T remove(int index) {
+        checkIndexForRemove(index);
+        T ret = data[index];
+        for (int i = index; i < size; i++) {
+            data[i] = data[i+1];
+        }
+        size--;
 
+        // 缩小容量
+        if (size == data.length / 4 && data.length / 2 != 0) {
+            resize(data.length / 2);
+        }
+
+        return ret;
     }
 
     // 删除第一个元素
     public T removeFirst() {
-
+        return remove(0);
     }
 
     // 删除末尾元素
     public T removeLast() {
-
+        return remove(size-1);
     }
 
     // 从数组中删除指定元素
     public void removeElement(T e) {
-
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
     }
 
     @Override
@@ -110,12 +148,25 @@ public class GenericArray<T> {
 
     // 扩容方法，时间复杂度 O(n)
     private void resize(int capacity) {
-
+        T[] temp = (T[]) new Object[capacity];
+        for (int i = 0; i < data.length; i++) {
+            temp[i] = data[i];
+        }
+        data = temp;
     }
 
     private void checkIndex(int index) {
         if (index < 0 || index > size) {
-            throw new IllegalArgumentException("Add failed! Require index >=0 and index <= size.");
+            throw new IllegalArgumentException("Add failed! Require index >= 0 and index <= size.");
+        }
+    }
+
+    private void checkIndexForRemove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Remove failed! Require index >= 0 and index < size.");
         }
     }
 }
+
+
+
