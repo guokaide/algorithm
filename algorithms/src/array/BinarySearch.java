53c1c72b9b721b2a9161a42adb083fde5b746f33
guokaide <kaideguo@gmail.com>
2018-11-07 18:36:56 +0800

    add comment in BinarySearch.java

--- algorithms/src/array/BinarySearch.java
+++ algorithms/src/array/BinarySearch.java
@@ -2,12 +2,13 @@ package array;
 
 public class BinarySearch {
 
+    // 迭代
     public static int binarySearch(int[] array, int target) {
         int lo = 0;
         int hi = array.length-1; // 始终在[lo, hi]范围内查找target
 
         while (lo <= hi) {
-            int mid = lo + ((hi - lo) >> 1);
+            int mid = lo + ((hi - lo) >> 1); // 这里若是 (lo + hi) / 2 有可能造成整型溢出
 
             if (array[mid] >  target) {
                 hi = mid - 1;
@@ -21,6 +22,7 @@ public class BinarySearch {
         return -1;
     }
 
+    // 递归
     public static int binarySearchRecur(int[] array, int target) {
         return bs(array, target, 0, array.length-1);
     }
