3c526d9208a67517df37cf7996a179a71214ab01
kai <kaideguo@gmail.com>
2018-11-08 16:45:16 +0800

    add solution 3

--- algorithms/src/array/BinarySearch.java
+++ algorithms/src/array/BinarySearch.java
@@ -1,7 +1,7 @@
 package array;
 
 public class BinarySearch {
-
+    // 最简单的二分查找算法：针对有序无重复元素数组
     // 迭代
     public static int binarySearch(int[] array, int target) {
         int lo = 0;
@@ -36,6 +36,99 @@ public class BinarySearch {
                 return bs(array, target, mid+1, hi);
             } else {
                 return mid;
+            }
+        }
+
+        return -1;
+    }
+
+    // 二分查找变形问题
+    // 1. 查找第一个值等于给定值的元素
+    public static int bsFirst(int[] array, int target) {
+        int lo = 0;
+        int hi = array.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + ((hi - lo) >> 1);
+
+            if (array[mid] > target) {
+                hi = mid - 1;
+            } else if (array[mid] < target) {
+                lo = mid + 1;
+            } else {
+                if (mid == lo || array[mid-1] != array[mid]) {
+                    return mid;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    // 2. 查找最后一个值等于给定值的元素
+    public static int bsLast(int[] array, int target) {
+        int lo = 0;
+        int hi = array.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + ((hi - lo) >> 1);
+
+            if (array[mid] > target) {
+                hi = mid - 1;
+            } else if (array[mid] < target) {
+                lo = mid + 1;
+            } else {
+                if (mid == hi || array[mid] != array[mid+1]) {
+                    return mid;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    // 3. 查找第一个大于等于给定值的元素
+    public static int bsFistGE(int[] array, int target) {
+        int lo = 0;
+        int hi = array.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + ((hi - lo) >> 1);
+
+            if (array[mid] >= target) {
+                if (mid == 0 || array[mid-1] < target) {
+                    return mid;
+                } else {
+                    hi = mid - 1;
+                }
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    // 4. 查找最后一个小于等于给定值的元素
+    public static int bsLastLE(int[] array, int target) {
+        int lo = 0;
+        int hi = array.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + ((hi - lo) >> 1);
+
+            if (array[mid] <= target) {
+                if (mid == hi || array[mid+1] > target) {
+                    return mid;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                hi = mid - 1;
             }
         }
 
